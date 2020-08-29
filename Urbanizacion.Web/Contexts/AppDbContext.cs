6556808d47e76946cd0bb45944e65f9edffe104b
johan altamirano <gohanalexanderdj@hotmail.com>
2020-08-28 23:32:27 -0500

    Conexion a base sql

---
+++ Urbanizacion.Web/Contexts/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Urbanizacion.Web.Data.Entities;
+
+namespace Urbanizacion.Web.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<TBL_CONJUNTO> TBL_CONJUNTO { get; set; }
+    }
+}
