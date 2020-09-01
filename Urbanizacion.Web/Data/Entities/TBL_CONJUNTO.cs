8ed864e110a5c320bc74fa460cd187b13bd35ce9
johan altamirano <gohanalexanderdj@hotmail.com>
2020-08-31 21:57:38 -0500

    cc

--- Urbanizacion.Web/Data/Entities/TBL_CONJUNTO.cs
+++ Urbanizacion.Web/Data/Entities/TBL_CONJUNTO.cs
@@ -11,13 +11,10 @@ namespace Urbanizacion.Web.Data.Entities
         [Key]
         public int CON_ID { get; set; }
 
-
         [MaxLength(100, ErrorMessage = "El {0} campo no puede tener mas de {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [Display(Name = "Nombre Urbanizacion")]
         public string CON_NOMBRE { get; set; }
-
-
 
         [MaxLength(13, ErrorMessage = "El {0} campo no puede tener mas de {1} caracteres.")]
         [MinLength(13, ErrorMessage = "El {0} campo no puede tener menos de {1} caracteres.")]
@@ -25,12 +22,11 @@ namespace Urbanizacion.Web.Data.Entities
         [Display(Name = "RUC")]
         public string CON_IDENTIFICACION { get; set; }
 
-
-
         [MaxLength(10, ErrorMessage = "El {0} campo no puede tener mas de {1} caracteres.")]
         [MinLength(10, ErrorMessage = "El {0} campo no puede tener menos de {1} caracteres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         [Display(Name = "Celular")]
         public string CON_TELEFONO { get; set; }
+
     }
 }
