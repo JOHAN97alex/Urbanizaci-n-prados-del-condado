1b93a01dc610dc84ee83cdc8824cb4f115083131
Xime Moreno <xime0389@hotmail.com>
2020-09-07 23:58:48 -0500

    Xmoreno Lista de propietarios

---
+++ Urbanizacion.Web/Controllers/PagosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Urbanizacion.Web.Contexts;
+using Urbanizacion.Web.Helpers;
+
+namespace Urbanizacion.Web.Controllers
+{
+    public class PagosController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IImageHelper _imageHelper;
+        private readonly IConverterHelper _converterHelper;
+
+        public PagosController(
+            AppDbContext context,
+            IImageHelper imageHelper,
+            IConverterHelper converterHelper)
+        {
+            _context = context;
+            _imageHelper = imageHelper;
+            _converterHelper = converterHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context
+                .TBL_PAGO
+                .Include(t => t.Propietario)
+                .OrderBy(t => t.PAG_FECHAPAGADO)
+                .ToListAsync());
+        }
+    }
+}
---
+++ Urbanizacion.Web/Controllers/PropietariosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Urbanizacion.Web.Contexts;
+using Urbanizacion.Web.Data.Entities;
+using Urbanizacion.Web.Helpers;
+
+namespace Urbanizacion.Web.Controllers
+{
+    public class PropietariosController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IImageHelper _imageHelper;
+        private readonly IConverterHelper _converterHelper;
+
+        public PropietariosController(
+            AppDbContext context,
+            IImageHelper imageHelper,
+            IConverterHelper converterHelper)
+        {
+            _context = context;
+            _imageHelper = imageHelper;
+            _converterHelper = converterHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context
+                .TBL_PROPIETARIO
+                .Include(t => t.Vehiculo)
+                .OrderBy(t => t.PRO_LOTE)
+                .ToListAsync());
+        }
+    }
+}
